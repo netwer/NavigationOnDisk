80ddfba8c6aae563c375420704ba891c2dba985c
art_net <art9207@gmail.com>
2015-08-05 10:04:41 +0300

    add project structure

---
+++ NavigationOnDisk/NavigationOnDiskApp/NavigationOnDiskUnitTestProject/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NavigationOnDiskUnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
